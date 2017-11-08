7b5b996521892259dd4d37f2f418f4da590726c7
BadisaMosesane <badisamosne@gmail.com>
2017-11-08 12:37:38 +0200

    Main and XY value classes for weight and height
    Visualize charts from user input

---
+++ XYValue.java
@@ -0,0 +1,30 @@
+
+/**
+ * Created by User on 5/11/2017.
+ */
+
+public class XYValue {
+    private double weight;
+    private double height;
+
+    public XYValue(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
